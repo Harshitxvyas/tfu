4e6f75b4a129f3a23f9138307b49c1636bd690f0
Harshitxvyas <harshitvyas454@gmail.com>
2025-07-04 14:05:04 +0530

    funnel fixed

--- app.py
+++ app.py
@@ -392,7 +392,7 @@ if side_radio == 'Platform Level Analysis':
         payment_df['Amount'] = payment_df['Amount'].astype(int)
         spend_df = spend_df.replace(-1, 0)
         spend_df['Category'] = spend_df.apply(categorize, axis=1)
-        spend_df.loc[(spend_df['CampaignName'].str.contains('_aj', na = False, case = False)),'Category'] = 'Astroji'
+        # spend_df.loc[(spend_df['CampaignName'].str.contains('_aj', na = False, case = False)),'Category'] = 'Astroji'
         # spend_df['Category'] = spend_df['CampaignName'].apply(lambda x: 'Spirituality' if ('_al' in str(x).lower() and '_tw' not in str(x).lower()) else ('Finance' if '_tw' in str(x).lower() else None))
         start_date2 = start_date
         start_date = pd.to_datetime(start_utc)
@@ -680,7 +680,8 @@ if side_radio == 'Funnels Analysis':
         spend_df = spend_df[spend_df['Platform'].fillna('').str.lower() == platform.lower()]
         payment_df['Amount'] = payment_df['Amount'].astype(int)
         spend_df = spend_df.replace(-1, 0)
-        spend_df['Category'] = spend_df['CampaignName'].apply(lambda x: 'Spirituality' if ('_al' in str(x).lower() and '_tw' not in str(x).lower()) else ('Finance' if '_tw' in str(x).lower() else None))
+        spend_df['Category'] = spend_df.apply(categorize, axis=1)
+        # spend_df['Category'] = spend_df['CampaignName'].apply(lambda x: 'Spirituality' if ('_al' in str(x).lower() and '_tw' not in str(x).lower()) else ('Finance' if '_tw' in str(x).lower() else None))
         # spend_df.loc[(spend_df['CampaignName'].str.contains('_aj|astroji|astro_ji',case = False, na=False)),'Category'] = 'Astroji'
         spend_df= spend_df[spend_df['Category'].fillna('').str.lower() == category.lower()]
         spend_df = create_funnel_table2(spend_df,platform)
